80dd1eae125c47b6a5ea7fee664a6ec35ca16a3a
MD Rifat Hossain <140188901+mohammadrifat-hossain@users.noreply.github.com>
2024-01-05 15:43:18 +0600

    Update connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () =>{
     try {
-        await mongoose.connect('process.env.MOMGO_URL',{
+        await mongoose.connect(process.env.MOMGO_URL,{
             serverSelectionTimeoutMS:30000
         })
     } catch (error) {
